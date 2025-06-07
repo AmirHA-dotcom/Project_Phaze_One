7ee819d3e74d996078160b50eae95afff194d983
Amir Hossein Abnous <amirhosseinabnous@gmail.com>
2025-06-07 10:11:48 +0330

    node creation

--- Circuit.cpp
+++ Circuit.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "Circuit.h"
+
+int Circuit::node_index_finder_by_name(const string& name)
+{
+    for (int i = 0; i < Nodes.size(); i++)
+    {
+        if (Nodes[i]->get_name() == name)
+            return i;
+    }
+    return -1;
+}
+
+void Circuit::create_new_resistor(string name, string node1_name, string node2_name, double resistance)
+{
+    int node1_index = node_index_finder_by_name(node1_name);
+    int node2_index = node_index_finder_by_name(node2_name);
+    if (node1_index == -1 || node2_index == -1)
+    {
+        cout << "One or both of the nodes can`t be found !!!" << endl;  // later add exception
+        return;
+    }
+    Elements.push_back(new Resistor(name, Nodes[node1_index], Nodes[node2_index], resistance));
+}
+
+void Circuit::create_new_capacitor(string name, string node1_name, string node2_name, double capacitance)
+{
+    int node1_index = node_index_finder_by_name(node1_name);
+    int node2_index = node_index_finder_by_name(node2_name);
+    if (node1_index == -1 || node2_index == -1)
+    {
+        cout << "One or both of the nodes can`t be found !!!" << endl;  // later add exception
+        return;
+    }
+    Elements.push_back(new Capacitor(name, Nodes[node1_index], Nodes[node2_index], capacitance));
+}
+void Circuit::create_new_inductor(string name, string node1_name, string node2_name, double inductance)
+{
+    int node1_index = node_index_finder_by_name(node1_name);
+    int node2_index = node_index_finder_by_name(node2_name);
+    if (node1_index == -1 || node2_index == -1)
+    {
+        cout << "One or both of the nodes can`t be found !!!" << endl;  // later add exception
+        return;
+    }
+    Elements.push_back(new Inductor(name, Nodes[node1_index], Nodes[node2_index], inductance));
+}
+
+void Circuit::create_new_node()
+{
+    string name = "DooDooL"; // should be change and add a counting logic
+    Nodes.push_back(new Node(name));
+}
--- Circuit.h
+++ Circuit.h
@@ -6,12 +6,26 @@
 #define PROJECT_PHAZE_ONE_CIRCUIT_H
 
 #include "Libraries.h"
+#include "Node.h"
+#include "Element.h"
+#include "Resistor.h"
+#include "Capacitor.h"
+#include "Inductor.h"
+#include "Voltage_Source.h"
 
 class Circuit
 {
 private:
-
+vector<Element*> Elements;
+vector<Node*> Nodes;
+public:
+    void create_new_resistor(string name, string node1_name, string node2_name, double resistance);
+    void create_new_capacitor(string name, string node1_name, string node2_name, double capacitance);
+    void create_new_inductor(string name, string node1_name, string node2_name, double inductor);
+    void create_new_voltage_sourse(string name, string node1_name, string node2_name, double voltage);
+    void create_new_node();
+private:
+    int node_index_finder_by_name(const string& name); // returns index, if not found, returns -1.
 };
-
 
 #endif //PROJECT_PHAZE_ONE_CIRCUIT_H
