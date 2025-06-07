7c37348016eb53d634f4d703c61e3fb9ff5991e2
Amir Hossein Abnous <amirhosseinabnous@gmail.com>
2025-06-07 18:13:10 +0330

    added constructor

--- Voltage_Source.h
+++ Voltage_Source.h
@@ -12,6 +12,7 @@ class Voltage_Source : public Element
 private:
     int aux_index;
 public:
+    Voltage_Source(string _name, Node* _node1, Node* _node2, double _value) : Element(_name, Element_Type::Voltage_Source, _node1, _node2, _value) {}
     void stamp(double time_step, vector<Triplet> &G_triplets, vector<double> &b, const vector<double> x_previous);
 };
 
