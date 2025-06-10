8bc31e9cd13d6eb138d1f0c0b8e2e5066f8be392
a <a>
2025-06-10 16:41:42 +0330

    better view! with some Controller

--- view.cpp
+++ view.cpp
@@ -706,12 +706,9 @@ bool View::handleFileMenu (Controller* C) {
 
 
     if (i.size() == 4 && i[0] == "add" && i[2] == "to" && i[3] == "circuits"){
-        for (auto u : i)
-            cout << u << "   ";
-        cout << endl;
-        cout << i.size() << endl;
         if (!C->validSchematicChoice(i[1]))
             throw invalidSchematicChoice();
+        cout << "odododod" << endl;
         C->addFileToCircuits(stoi(i[1]));
     }
     if (isDigit(line) && C->validSchematicChoice(line)) {
